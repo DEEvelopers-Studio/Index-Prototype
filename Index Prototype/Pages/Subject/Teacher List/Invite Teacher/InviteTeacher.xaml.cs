0ebc1cdade308e68c34f2aa0423cceca081fb857
JRMDEEz <dryestboot@gmail.com>
2022-11-28 23:40:39 +0800

    Invite teacher UI added

---
+++ Index Prototype/Pages/Subject/Teacher List/Invite Teacher/InviteTeacher.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Index_Prototype.Pages.Subject.Teacher_List.Invite_Teacher
+{
+    /// <summary>
+    /// Interaction logic for InviteTeacher.xaml
+    /// </summary>
+    public partial class InviteTeacher : Window
+    {
+        public InviteTeacher()
+        {
+            InitializeComponent();
+        }
+    }
+}
