ec1a7d43d340f688e19bb48de13c4a56d6433792
JRMDEEz <dryestboot@gmail.com>
2022-12-06 15:39:38 +0800

    initial

---
+++ Index Prototype/Directory/Converters/PasswordVisibleConverter.cs
@@ -0,0 +1,26 @@
+﻿using MahApps.Metro.IconPacks;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Index_Prototype.Directory.Converters
+{
+    [ValueConversion(typeof(string), typeof(PackIconControl))]
+    internal class PasswordVisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? PackIconFontAwesomeKind.EyeSlashSolid : PackIconFontAwesomeKind.EyeRegular;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            return (PackIconFontAwesomeKind)value == PackIconFontAwesomeKind.EyeSlashSolid;
+        }
+    }
+}
