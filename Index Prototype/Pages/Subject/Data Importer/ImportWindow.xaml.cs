b8c2bb68a17658f9721439a765b3d3b001ebfcb6
JRMDEEz <dryestboot@gmail.com>
2023-01-12 07:33:46 +0800

    minor changes

--- Index Prototype/Pages/Subject/Data Importer/ImportWindow.xaml.cs
+++ Index Prototype/Pages/Subject/Data Importer/ImportWindow.xaml.cs
@@ -57,35 +57,45 @@ namespace Index_Prototype.Pages.Subject.Data_Importer
                 Close();
                 return;
             }
+            try
+            {
                 Dictionary<string, string> studentsRaw = new Dictionary<string, string>();
                 Worksheet xlWorksheet = wb.Sheets[1];
                 string tst = wb.Sheets[1].Name;
                 Range namesColumn = xlWorksheet.UsedRange.Columns[NameColumnTargetBox.Text];
                 Range uidColumn = xlWorksheet.UsedRange.Columns[StudIDColumnTargetBox.Text];
                 bool isSectionIncluded = !string.IsNullOrWhiteSpace(SectionColBox.Text);
-                Range sectionColumn = isSectionIncluded?xlWorksheet.UsedRange.Columns[SectionColBox.Text]:null;
+                Range sectionColumn = isSectionIncluded ? xlWorksheet.UsedRange.Columns[SectionColBox.Text] : null;
                 string[] namesArray = ((System.Array)namesColumn.Cells.Value).OfType<object>().Select(o => o.ToString()).ToArray();
                 string[] uidArray = ((System.Array)uidColumn.Cells.Value).OfType<object>().Select(o => o.ToString()).ToArray();
-                string[] sectionArray = sectionColumn != null?((System.Array)sectionColumn.Cells.Value).OfType<object>().Select(o => o.ToString()).ToArray():null;
+                string[] sectionArray = sectionColumn != null ? ((System.Array)sectionColumn.Cells.Value).OfType<object>().Select(o => o.ToString()).ToArray() : null;
                 int shortestRow = uidArray.Length > namesArray.Length ? namesArray.Length : uidArray.Length;
                 int failedImports = 0;
-                int startReadingat = int.Parse(StartReadingRow.Text) -1;
-            if (startReadingat > shortestRow || startReadingat < 0) { MessageBox.Show("Invalid row start count"); wb?.Close(); return; }
+                int startReadingat = int.Parse(StartReadingRow.Text) - 1;
+                if (startReadingat > shortestRow || startReadingat < 0) { MessageBox.Show("Invalid row start count"); wb?.Close(); return; }
                 for (int i = startReadingat; i < shortestRow; i++)
                 {
-                string uid = uidArray[i];
-                string name = namesArray[i];
-                string seciton = i < (sectionArray?.Length??0) ? sectionArray?[i] : null ;
+                    string uid = uidArray[i];
+                    string name = namesArray[i];
+                    string seciton = i < (sectionArray?.Length ?? 0) ? sectionArray?[i] : null;
                     if (name == null || uid == null) { failedImports++; continue; }
-                if (uid.Length == 10) uid = "0"+uid;
+                    if (uid.Length == 10) uid = "0" + uid;
                     if (uid.Length != 11) { failedImports++; continue; }
-                string[] parsedName = name.Split(new char[] {','}, 2);
-                if(parsedName.Length < 2) { failedImports++; continue; }
-                    DatabaseHelper.PutStudent(new Directory.DataTemplates.Student() { firstName = parsedName[1].Trim(),lastName = parsedName[0].Trim(), uid=uid,section= seciton });
-                    if(subjectId != null) DatabaseHelper.AddStudenttoSubject( uid, subjectId);
+                    string[] parsedName = name.Split(new char[] { ',' }, 2);
+                    if (parsedName.Length < 2) { failedImports++; continue; }
+                    DatabaseHelper.PutStudent(new Directory.DataTemplates.Student() { firstName = parsedName[1].Trim(), lastName = parsedName[0].Trim(), uid = uid, section = seciton });
+                    if (subjectId != null) DatabaseHelper.AddStudenttoSubject(uid, subjectId);
                 }
 
-            MessageBox.Show(failedImports == 0 ? "Data Imported Successfully" : $"Data Imported with {failedImports} failure");
+                MessageBox.Show(failedImports == 0 ? "Data Imported Successfully" : $"Data Imported with {failedImports} failure");
+            }
+            catch (System.Exception)
+            {
+                MessageBox.Show("Problem Reading the file");
+                throw;
+            }
+                
+
             wb?.Close();
             Close();
             //Range xlRange = xlWorksheet.UsedRange;
